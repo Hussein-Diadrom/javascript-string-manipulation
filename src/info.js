7bde15c26159d6878aa0819f9c97eec613df2e72
Hussein-Diadrom <95290186+Hussein-Diadrom@users.noreply.github.com>
2021-12-10 10:50:10 +0100

    Update info.js

--- src/info.js
+++ src/info.js
@@ -9,9 +9,9 @@ Options:
 
 [CR|LF|CRLF]: Desired line ending
 
-          CR is a carriage return character \r (rare)
-          LF is a line feed character \n (common on Unix and macOS)
-          CRLF is a combined ending \r\n(common on Windows)
+          CR is a carriage return character \\r (rare)
+          LF is a line feed character \\n (common on Unix and macOS)
+          CRLF is a combined ending \\r\\n(common on Windows)
         
 Example:
 
